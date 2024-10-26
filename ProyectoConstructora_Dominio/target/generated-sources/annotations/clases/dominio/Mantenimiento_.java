daa27c4ebeb02b22ad6056d5e74ebc8a3126ca8a
Rodrigo Gtz <rigo_22mayo@hotmail.com>
2024-10-26 02:51:26 -0700

    Fix small
    Pequelos arreglos de dependencias

--- ProyectoConstructora_Dominio/target/generated-sources/annotations/clases/dominio/Mantenimiento_.java
+++ ProyectoConstructora_Dominio/target/generated-sources/annotations/clases/dominio/Mantenimiento_.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-26T02:46:19", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-26T02:49:50", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Mantenimiento.class)
 public class Mantenimiento_ { 
 
