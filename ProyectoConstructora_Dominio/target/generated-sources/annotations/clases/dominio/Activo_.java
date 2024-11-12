e1a2073cb0d9a5b458a55c01dc60154c0fe48aba
Rodrigo Gtz <rigo_22mayo@hotmail.com>
2024-11-12 11:57:38 -0700

    Clean and build
    Clean and build

--- ProyectoConstructora_Dominio/target/generated-sources/annotations/clases/dominio/Activo_.java
+++ ProyectoConstructora_Dominio/target/generated-sources/annotations/clases/dominio/Activo_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-31T00:21:19", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-12T11:56:15", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Activo.class)
 public class Activo_ { 
 
